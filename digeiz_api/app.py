59f0dfc2ec0fcab0e81ef2342bed58f398eb54df
Mihamina Rakotovazaha <r.miham@yahoo.com>
2021-12-14 22:35:42 +0100

    feat: allow to create an account

--- digeiz_api/app.py
+++ digeiz_api/app.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 
 app = Flask(__name__)
 app.secret_key = "4CE1E6FD-BD68-4E73-B920-5EE95FD03FEC"
 api = Api(app)
 
-accounts = [
-    {
-        'name': 'some_account',
-        'id': 1
-    }
-]
+accounts = []
 
 malls = [
     {
@@ -45,9 +40,37 @@ class UnitList(Resource):
         """Get the account list method."""
         return {'units': units}
 
+class AddAccount(Resource):
+    """Create a new account from /account endpoint."""
+    
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    
+    def post(self):
+        """Create an account if not existing."""
+        
+        data = AddAccount.parser.parse_args()
+        if next(filter(lambda x: x['name'] == data['name'], accounts), None) is not None:
+            return {'message': "An account with the name '{}' already exists".format(data['name'])}
+
+        account = {'name': data['name'], 'password': data['password']}
+        accounts.append(account)
+
+        return account, 201
+
 api.add_resource(AccountList, '/account')
 api.add_resource(MallList, '/mall')
 api.add_resource(UnitList, '/unit')
+api.add_resource(AddAccount, '/account')
 
 if __name__ == '__main__':
     app.run(debug=True)
