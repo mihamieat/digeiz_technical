0ddd69a6e8fc5de368deac74369b93639a698029
--local <--local>
2021-12-26 20:36:26 +0100

    tests: update tests

--- test/models/account.py
+++ test/models/account.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 """test account model."""
+import pytest
+
+from app.models.account import AccountModel
+
+
+@pytest.fixture
+def account():
+    """Test account model."""
+    account = AccountModel("some_name", "some_location", "7cbb0b7e-667b-11ec-a387-acde48001122")
+    
